e140fe2bc90ab454cb6e36e936d48c6dba390285
Nika <kameeri@icloud.com>
2020-08-19 15:48:33 +0000

    kadai1

---
+++ practice.php
@@ -0,0 +1,34 @@
+<?php
+echo 'hello php!';
+
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+echo $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10", "11月", "12月"];
+echo $array_mouth[7];
+
+$hello = "Hello,";
+$name = "Yu's World!";
+echo $hello . $name;
+
+$tech_boost = 'tech';
+$tech_boost .= ' boost';
+echo $tech_boost;
+
+$_2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+echo $_2018_calendar["December"];
