bdb35de39bd2ca8434bd7de1d8a68e20a7ecdc7c
Nika <kameeri@icloud.com>
2020-08-24 16:12:50 +0000

    kadai2

--- practice.php
+++ practice.php
@@ -1,20 +1,20 @@
 <?php
-echo 'hello php!';
+echo 'hello php!' . "\n";
 
 $a = 3;
 $b = 7;
-echo $a + $b;
+echo $a + $b . "\n";
 
-echo $array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10", "11月", "12月"];
-echo $array_mouth[7];
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10", "11月", "12月"];
+echo $array_month[7] . "\n";
 
 $hello = "Hello,";
 $name = "Yu's World!";
-echo $hello . $name;
+echo $hello . $name . "\n";
 
 $tech_boost = 'tech';
 $tech_boost .= ' boost';
-echo $tech_boost;
+echo $tech_boost . "\n";
 
 $_2018_calendar = [
   "January" => "1月",
@@ -31,4 +31,35 @@ $_2018_calendar = [
   "December" => "12月"
 ];
 
-echo $_2018_calendar["December"];
+echo $_2018_calendar["December"] . "\n";
+
+
+
+
+$name = "Yu";
+if($name = "Yu") {
+  echo "私は　あなたの名前です" . "\n";
+} else {
+  echo "あなたの名前ではありません" . "\n";
+}
+
+
+$total = 0;
+for ($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total . "\n";
+
+
+
+$fruits = array("apple", "orange", "peach", "kiwi", "banana");
+foreach($fruits as $fruits) {
+  echo "要素は" . $fruits . "\n";
+}
+
+
+for($i = 1; $i <= 100; $i++){
+  if(($i % 5) == 0){
+    echo $i . "\n";
+  }
+}
